6282ce48e4b880afea3233dc85fce2595aa6d528
wang zhen yu <asdfg10510588@gmail.com>
2021-01-11 19:52:46 +0800

    引入後台的css, js資源檔, 新增後台的route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\AdminPostsController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,14 @@ use Illuminate\Support\Facades\Route;
 //})->name('home');
 
 Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.dashboard.index');
+
+    Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');
+    Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');
+    Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
